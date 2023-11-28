0414305144adf00100da8c3385dc2b839cb52f07
Jahan <148367918+jahan-wp@users.noreply.github.com>
2023-11-28 19:14:44 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// let btn = document.getElementById("btn")
+let entriesP = document.getElementById("entry")
+let count = 0
+
+function clickMe() {
+  count += 1
+  entriesP.textContent = count
+
+}
